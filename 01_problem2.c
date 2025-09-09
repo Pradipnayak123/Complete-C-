4651b248519cdf021022e73ee5772e139f17a166
Pradipnayak123 <pradipn962@gmail.com>
2025-09-09 17:53:23 +0530

    Add files via upload

---
+++ 01_problem2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int r = 1;
+    int h =2;
+    printf("The area of the circle of radius %d is %f\n",r,3.14*r*r);
+    printf("The volume of the clynder of radius %d and h %d is %f",r,h,3.14*r*r*h);
+    return 0;
+}
